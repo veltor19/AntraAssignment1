d5825fa5e194c6cbcfa63e6e8775a2282d26165e
Ryan Cheung <143753964+veltor19@users.noreply.github.com>
2025-10-30 02:13:15 -0400

    Add files via upload

---
+++ C# Day 2 Assignment/Color.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Day_2_Assignment {
+    internal class Color {
+        private int red;
+        private int green;
+        private int blue;
+        private int alpha;
+        public int Red {
+            get {
+                return red;
+            }
+            set {
+                if (value < 0 || value > 255) {
+                    throw new ArgumentOutOfRangeException("Red value must be between 0 and 255.");
+                }
+                red = value;
+            }
+        }
+        public int Green {
+            get {
+                return green;
+            }
+            set {
+                if (value < 0 || value > 255) {
+                    throw new ArgumentOutOfRangeException("Green value must be between 0 and 255.");
+                }
+                green = value;
+            }
+        }
+        public int Blue {
+            get {
+                return blue;
+            }
+            set {
+                if (value < 0 || value > 255) {
+                    throw new ArgumentOutOfRangeException("Blue value must be between 0 and 255.");
+                }
+                blue = value;
+            }
+        }
+        public int Alpha {
+            get {
+                return alpha;
+            }
+            set {
+                if (value < 0 || value > 255) {
+                    throw new ArgumentOutOfRangeException("alpha value must be between 0 and 255.");
+                }
+                alpha = value;
+            }
+        }
+
+        public Color(int red, int green, int blue) {
+            Red = red;
+            Green = green;
+            Blue = blue;
+            Alpha = 255;
+        }
+
+        public int GrayScale() {
+            return (red + green + blue) / 3;
+        }
+
+    }
+}
---
+++ C# Day 2 Assignment/Program.cs
@@ -0,0 +1,53 @@
+﻿using C__Day_2_Assignment; // Add missing semicolon and fix syntax for using directive
+/*
+int[] GenerateArray(int size) {
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++) {
+        array[i] = i + 1;
+    }
+    return array;
+}
+
+void ReverseArray(int[] array) {
+    int left = 0;
+    int right = array.Length - 1;
+    while (left < right) {
+        int temp = array[left];
+        array[left] = array[right];
+        array[right] = temp;
+        left++;
+        right--;
+    }
+}
+
+void PrintArray(int[] array) {
+    foreach (var item in array) {
+        Console.Write(item + " ");
+    }
+}
+
+int[] array = GenerateArray(10);
+ReverseArray(array);
+PrintArray(array);
+*/
+/*
+int Fibonacci(int n) {
+    if (n <= 1) {
+        return n;
+    }
+    return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+for (int i = 1; i <= 10; i++) {
+    Console.WriteLine($"Fibonacci({i}) = {Fibonacci(i)}");
+}
+*/
+
+
+Color color = new Color(255, 0, 0);
+Ball ball = new Ball(5, color);
+ball.Throw();
+Console.WriteLine(ball.GetThrowCount());
+ball.Pop();
+ball.Size = 10;
+Console.WriteLine(ball.Size);
