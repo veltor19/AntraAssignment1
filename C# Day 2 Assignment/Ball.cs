a06de66f95a9273e6ee28d2f04094eaca18b76af
Ryan Cheung <143753964+veltor19@users.noreply.github.com>
2025-10-30 02:11:44 -0400

    Add files via upload

---
+++ C# Day 2 Assignment/Ball.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Day_2_Assignment {
+    internal class Ball {
+        private int size;
+        private Color color;
+        private int throwCount;
+
+        public Ball(int size, Color color) {
+            this.size = size;
+            this.color = color;
+            this.throwCount = 0;
+        }
+
+        public int Size {
+            get { return size; }
+            set { size = value; }
+        }
+
+        public Color Color {
+            get { return color; }
+            set { color = value; }
+        }
+
+        public void Pop() {
+            size = 0;
+        }
+
+        public void Throw() {
+            if (size > 0) {
+                throwCount++;
+            } else {
+                Console.WriteLine("Cannot throw a popped ball.");
+            }
+        }
+
+        public int GetThrowCount() {
+            return throwCount;
+        }
+    }
+}
